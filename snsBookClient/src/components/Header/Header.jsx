9c6b6da6f8787e8ddb1882cc00731e0e5f225138
LeSheon <dlelfo7@gmail.com>
2024-05-27 13:45:09 -0400

    add logo searchbar header

--- snsBookClient/src/components/Header/Header.jsx
+++ snsBookClient/src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import "./header.scss";
+import sbLogo from "../../assets/logo/SNS_Book_Logo.svg";
 
 export default function Header() {
 
     return (
         <header className="header">
-            <div>This is Header</div>    
+            <img className="header__logo" src={sbLogo} alt="SNS Book Logo"/>
+            <div className="header__search-container">
+                <input className="header__searchbar" type="text" />
+            </div>
+            <div className="header__feature"></div>
         </header>
     )
 }
